5648efcb8dffed1f580ac2252acfeb4859f0eb9d
chenlingyun110 <1095806859@qq.com>
2017-11-16 11:19:37 +0800

    update

---
+++ src/demo/aaaaa.java
@@ -0,0 +1,19 @@
+/**
+ * Title: aaaaa.java
+ * Description: 
+ * Copyright: Copyright (c) 2017
+ * Company: Shanghai Huateng Software Systems Co., Ltd.
+*/
+
+package demo;
+
+/**
+* @Description: 
+ * @author: chenly
+ * @date: 2017年11月16日
+*/
+
+public class aaaaa {
+	//fuck!
+
+}
