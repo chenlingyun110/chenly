05c3fab2282041477eba276a5e95fb1644c4c2ba
chenlingyun110 <1095806859@qq.com>
2017-11-16 11:41:39 +0800

    cly update

---
+++ src/demo/whatthefuck.java
@@ -0,0 +1,18 @@
+/**
+ * Title: whatthefuck.java
+ * Description: 
+ * Copyright: Copyright (c) 2017
+ * Company: Shanghai Huateng Software Systems Co., Ltd.
+*/
+
+package demo;
+
+/**
+* @Description: 
+ * @author: chenly
+ * @date: 2017年11月16日
+*/
+
+public class whatthefuck {
+	//i'm happy
+}
