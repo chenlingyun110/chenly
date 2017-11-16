c0b9f829ea536c337a7fb53bdfe58c1b7b68855e
chenlingyun110 <1095806859@qq.com>
2017-11-16 11:38:46 +0800

    ainter

---
+++ src/demo/AInter.java
@@ -0,0 +1,18 @@
+/**
+ * Title: AInter.java
+ * Description: 
+ * Copyright: Copyright (c) 2017
+ * Company: Shanghai Huateng Software Systems Co., Ltd.
+*/
+
+package demo;
+
+/**
+* @Description: 
+ * @author: chenly
+ * @date: 2017年11月16日
+*/
+
+public interface AInter {
+	//final
+}
